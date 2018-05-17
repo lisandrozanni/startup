1a948018b91e1e6e88228762c9f2e015e050b7f8
lisandrozanni <lisandrozanni@hotmail.com>
2018-05-16 22:45:53 -0300

    Delete App.js

--- 01-JS-DOM-APIs/js/App.js
+++
@@ -1,7 +0,0 @@
-function see() {
-    alert("Hello Word");
-}
-
-// { "type": "success", "value": { "id": 159, "joke": "Chuck Norris did in fact, build Rome in a day.", "categories": [] } }
-var response1 = (id) => ({"type": "success", "value": {id: id, "joke": "Chuck Norris did in fact, build Rome in a day.", "categories": []}});
-
