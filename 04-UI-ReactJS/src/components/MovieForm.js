e35ca7363202401ef37775b352d96afe5f72f382
lisandrozanni <lisandrozanni@hotmail.com>
2018-05-28 12:45:23 -0300

    In upgrade to use with Redux
    Bootstrap is used to facilitate the style and responsive design.

---
+++ 04-UI-ReactJS/src/components/MovieForm.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+class MovieForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      year: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.onAddMovie(this.state);
+    this.setState({
+      title: '',
+      year: ''
+    });
+  }
+
+  handleInputChange(e) {
+    const {value, name} = e.target;
+    console.log(value, name);
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <form onSubmit={this.handleSubmit} className="card-body">
+          <div className="form-group">
+            <input
+              type="text"
+              name="title"
+              className="form-control"
+              value={this.state.title}
+              onChange={this.handleInputChange}
+              placeholder="Title"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="year"
+              className="form-control"
+              value={this.state.year}
+              onChange={this.handleInputChange}
+              placeholder="Year"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default MovieForm;
